72ad951a5318104af53b40feda255d720aea3fe6
James Anderson <jrsa@jrsa.co>
2017-06-07 02:41:53 -0700

    doesn't update current pot -> cv out, updating only happens on trigger interrupt int0, also need dac output to be tied to hardware timer, adc scan works tho

--- seq.c
+++ seq.c
@@ -19,6 +19,8 @@
 
 #define DEBUG_LED PD0
 
+#define N_ADC_CHANNELS 6
+
 void int0_init();
 void adc_init();
 uint16_t adc_read(uint8_t ch);
@@ -36,49 +38,57 @@ void int0_init() {
 }
 
 void dac_init() {
-  SPI_DDR |= (1 << SPI_MOSI) | (1 << SPI_SCLK); //SCK, MOSI are output
-  DAC_CS_DDR |= (1 << DAC_CS); //CS for MCP4922 DAC
-  DAC_CS_PORT |= (1 << DAC_CS); //pull CS high to intialize 
-  
-  SPI_DDR &= ~(1 << SPI_MISO); // MISO is input
-  
-  //SPI enable, Master Mode, F_OSC/4, interrupt enabled
-  SPCR = (1 << SPE) | (1 << MSTR) | (0 << SPR1)| (0 << SPR0); 
-  SPSR = (1 << SPI2X); //SPI double speed = 2MHz
+    SPI_DDR |= (1 << SPI_MOSI) | (1 << SPI_SCLK); //SCK, MOSI are output
+    DAC_CS_DDR |= (1 << DAC_CS); //CS for MCP4922 DAC
+    DAC_CS_PORT |= (1 << DAC_CS); //pull CS high to intialize 
 
-  dac_write(0, 0);
-  dac_write(1, 0);
+    SPI_DDR &= ~(1 << SPI_MISO); // MISO is input
+
+    //SPI enable, Master Mode, F_OSC/4, interrupt enabled
+    SPCR = (1 << SPE) | (1 << MSTR) | (0 << SPR1)| (0 << SPR0); 
+    SPSR = (1 << SPI2X); //SPI double speed = 2MHz
+
+    dac_write(0, 0);
+    dac_write(1, 0);
 }
 
 void adc_init() {
     ADMUX = (1 << REFS0);
-    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+    ADCSRA = (1 << ADEN)  // enable adc peripheral
+           | (1 << ADPS2) // prescaler bits
+           | (1 << ADPS1)
+           | (1 << ADPS0)
+           | (1 << ADATE) // autotrigger enable
+           | (1 << ADIE); // interrupt enable
+    ADCSRB &= 0xf8; // clear lower three bits to enable free running adc mode
+    // should continuously convert and generate interrupts after each conversion
+
 }
 
 void dac_write(uint8_t channel, uint16_t data) {
-  DAC_CS_PORT &= ~(1 << DAC_CS);  //pull CS low to enable DAC
-  
-  // send configuration bits and highest 4 data bits
-  SPDR = (channel<<MCP4921_ABSEL) 
-          | (1 << MCP4921_BUF) 
-          | (1 << MCP4921_GAIN) 
-          | (1 << MCP4921_SHDN) 
-          | ((data>>8) & 0x0F);
+    DAC_CS_PORT &= ~(1 << DAC_CS);  //pull CS low to enable DAC
+    
+    // send configuration bits and highest 4 data bits
+    SPDR = (channel<<MCP4921_ABSEL) 
+            | (1 << MCP4921_BUF) 
+            | (1 << MCP4921_GAIN) 
+            | (1 << MCP4921_SHDN) 
+            | ((data>>8) & 0x0F);
 
-  while (!(SPSR & (1 << SPIF))) // wait
-    ;
+    while (!(SPSR & (1 << SPIF))) // wait
+      ;
 
-  SPDR = data & 0x00FF; // send lo byte
+    SPDR = data & 0x00FF; // send lo byte
 
-  while (!(SPSR & (1 << SPIF))) // wait
-    ;
+    while (!(SPSR & (1 << SPIF))) // wait
+      ;
 
-  DAC_CS_PORT |= (1 << DAC_CS);   //pull CS high to latch data
+    DAC_CS_PORT |= (1 << DAC_CS);   //pull CS high to latch data
 }
- 
+
 uint16_t adc_read(uint8_t ch) {
     ch &= 0b00000110; // limit to 6 channels, atmega8 has 6 adc pins
-    ADMUX = (ADMUX & 0xF8) | ch;     // clears the bottom 3 bits before ORing
+    ADMUX = (ADMUX & 0xF8) | ch; // clears the bottom 3 bits before ORing
  
     // start single conversion
     ADCSRA |= (1 << ADSC);
@@ -91,8 +101,28 @@ uint16_t adc_read(uint8_t ch) {
     return (ADC);
 }
 
+uint16_t adc_in[16] = {0,};
+uint16_t cv_out = 0; // 12 bit dac output
+uint8_t stage = 0;
+
 ISR(INT0_vect){
-    PIND = (1 << DEBUG_LED); // toggle debug led
+    cv_out = adc_in[stage];
+    stage++;
+    if(stage == N_ADC_CHANNELS) {
+        stage = 0;
+    }
+}
+
+uint8_t adc_scan_index = 0;
+ISR(ADC_vect) {
+    adc_in[adc_scan_index] = (ADCL | (ADCH << 8) << 2);
+    adc_scan_index++;
+    if(adc_scan_index == N_ADC_CHANNELS) {
+        PIND = (1 << DEBUG_LED); // toggle debug led
+        adc_scan_index = 0;
+    }
+    ADMUX &= 0xF0; // clear mux selection
+    ADMUX |= (adc_scan_index & 0xf); // new mux selection
 }
 
 
@@ -102,6 +132,10 @@ void main() {
     int0_init();
 
     DDRD |= (1 << DEBUG_LED); // enable debug led output
-    
-    while(1);
+
+    ADCSRA |= (1 << ADSC); // start adc
+
+    while(1) {
+        dac_write(0, cv_out);
+    }
 }
